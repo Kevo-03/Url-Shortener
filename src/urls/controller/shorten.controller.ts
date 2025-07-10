1b9d51aac471dfbfd6c5f14ee4a1e09760a84184
Kıvanç Onat Türker <kivo0326@gmail.com>
2025-07-10 11:25:23 +0300

    shorten controller post method

--- src/urls/controller/shorten.controller.ts
+++ src/urls/controller/shorten.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Post, Get } from '@nestjs/common';
+import { Controller, Post, Get, Body } from '@nestjs/common';
 import { UrlsService } from '../urls.service';
+import { CreateUrlDto } from '../dto/create-url.dto';
+
+const BASE_URL = process.env.BASE_URL ?? 'http://localhost:3000';
 
 @Controller('shorten')
 export class ShortenController {
     constructor(private urlsService: UrlsService) { }
 
-
+    @Post()
+    async shorten(@Body() body: CreateUrlDto) {
+        const shortCode = await this.urlsService.shorten(body.url);
+        return { shortUrl: `${BASE_URL}/${shortCode}`, shortCode };
+    }
 }
