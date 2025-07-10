23a62c19ea5ef2e298ea234a98bcca7f555064cc
Kıvanç Onat Türker <kivo0326@gmail.com>
2025-07-10 11:33:27 +0300

    UrlsRepository dependency fix on UrlsModule

--- src/urls/urls.module.ts
+++ src/urls/urls.module.ts
@@ -3,9 +3,10 @@ import { UrlsController } from './urls.controller';
 import { UrlsService } from './urls.service';
 import { ShortenController } from './controller/shorten.controller';
 import { RedirectController } from './controller/redirect.controller';
+import { UrlsRepository } from './urls.repository';
 
 @Module({
     controllers: [UrlsController, ShortenController, RedirectController],
-    providers: [UrlsService]
+    providers: [UrlsService, UrlsRepository]
 })
 export class UrlsModule { }
