55ad623691a972bad1a59c1f91568f9ed5da9aeb
Kıvanç Onat Türker <kivo0326@gmail.com>
2025-07-10 10:39:34 +0300

    service implementation

--- src/urls/controller/shorten.controller.ts
+++ src/urls/controller/shorten.controller.ts
@@ -1,7 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get } from '@nestjs/common';
 import { UrlsService } from '../urls.service';
 
 @Controller('shorten')
 export class ShortenController {
     constructor(private urlsService: UrlsService) { }
+
+
 }
