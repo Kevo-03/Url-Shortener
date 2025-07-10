8e2ba54a122d6d1c135b7d25252454a5d3cb5397
Kıvanç Onat Türker <kivo0326@gmail.com>
2025-07-10 09:26:27 +0300

    shortening and redirect controllers created

--- src/urls/urls.module.ts
+++ src/urls/urls.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UrlsController } from './urls.controller';
 import { UrlsService } from './urls.service';
+import { ShortenController } from './controller/shorten.controller';
+import { RedirectController } from './controller/redirect.controller';
 
 @Module({
-    controllers: [UrlsController],
+    controllers: [UrlsController, ShortenController, RedirectController],
     providers: [UrlsService]
 })
 export class UrlsModule { }
