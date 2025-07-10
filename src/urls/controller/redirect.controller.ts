a8fe372a68f7f9e6f8894a6fb915a6bd36e45646
Kıvanç Onat Türker <kivo0326@gmail.com>
2025-07-10 13:09:08 +0300

    redirect controller implementation

--- src/urls/controller/redirect.controller.ts
+++ src/urls/controller/redirect.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param, Res } from '@nestjs/common';
 import { UrlsService } from '../urls.service';
 import { Response } from 'express';
 
@@ -7,7 +7,11 @@ export class RedirectController {
     constructor(private urlsService: UrlsService) { }
 
     @Get(':code')
-    async redirect(@Param('code') code: string) {
-
+    async redirect(@Param('code') shortUrl: string, @Res() res: Response) {
+        const longUrl = await this.urlsService.resolve(shortUrl);
+        if (!longUrl) {
+            throw new NotFoundException('Short URL not found');
+        }
+        return res.redirect(301, longUrl);
     }
 }
