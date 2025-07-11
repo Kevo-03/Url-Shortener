54dd8ba1d6141e1775e29f8242489e832863cdee
Kıvanç Onat Türker <kivo0326@gmail.com>
2025-07-11 10:59:20 +0300

    v1 controller

--- src/urls/urls.module.ts
+++ src/urls/urls.module.ts
@@ -3,9 +3,10 @@ import { UrlsService } from './urls.service';
 import { ShortenController } from './controller/shorten.controller';
 import { RedirectController } from './controller/redirect.controller';
 import { UrlsRepository } from './repository/urls.repository';
+import { UrlsControllerV1 } from './controller/v1/urls.controller';
 
 @Module({
-    controllers: [ShortenController, RedirectController],
+    controllers: [ShortenController, RedirectController, UrlsControllerV1],
     providers: [UrlsService, UrlsRepository]
 })
 export class UrlsModule { }
