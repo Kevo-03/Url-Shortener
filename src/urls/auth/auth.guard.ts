c4611a36ae2393c66d7f72ae8c8a8f5da64ae715
Kıvanç Onat Türker <kivo0326@gmail.com>
2025-07-14 20:42:07 +0300

    fix: add basic auth

---
+++ src/urls/auth/auth.guard.ts
@@ -0,0 +1,33 @@
+import {
+    CanActivate, ExecutionContext, Injectable, UnauthorizedException,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { ENV_VAR } from '../../config/app.config';
+import { Buffer } from 'buffer';
+
+@Injectable()
+export class BasicAuthGuard implements CanActivate {
+    canActivate(ctx: ExecutionContext) {
+        const req = ctx.switchToHttp().getRequest<Request>();
+        const header = req.headers.authorization;
+
+        if (!header?.startsWith('Basic ')) {
+            throw new UnauthorizedException('Missing Authorization header');
+        }
+
+        const [user, pass] = Buffer
+            .from(header.slice(6), 'base64')
+            .toString('utf8')
+            .split(':', 2);
+
+        if (user === ENV_VAR.BASIC_AUTH_USER && pass === ENV_VAR.BASIC_AUTH_PASS) {
+            return true;
+        }
+        throw new UnauthorizedException({
+            statusCode: 401,
+            message: 'Invalid basic credentials',
+            error: 'Unauthorized',
+            headers: { 'WWW-Authenticate': 'Basic realm="ShortURL"' },
+        });
+    }
+}
--- src/urls/urls.module.ts
+++ src/urls/urls.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UrlsService } from './urls.service';
-import { ShortenController } from './controller/shorten.controller';
-import { RedirectController } from './controller/redirect.controller';
 import { UrlsRepository } from './repository/urls.repository';
 import { UrlsControllerV1 } from './controller/v1/urls.controller';
 
 @Module({
-    controllers: [ShortenController, RedirectController, UrlsControllerV1],
+    controllers: [UrlsControllerV1],
     providers: [UrlsService, UrlsRepository]
 })
 export class UrlsModule { }
