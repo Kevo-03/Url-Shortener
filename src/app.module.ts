eff99b4496eecac29ecb9030ccadc7c9675eb36e
Kıvanç Onat Türker <kivo0326@gmail.com>
2025-07-16 14:44:13 +0300

    redis config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 import { UrlsModule } from './urls/urls.module';
 import { RedisModule, RedisModuleOptions } from '@nestjs-modules/ioredis';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { ENV_VAR } from './config/app.config';
+import { REDIS_CONNECTION_URL } from './config/redis.config';
 
 
 @Module({
@@ -20,7 +20,7 @@ import { ENV_VAR } from './config/app.config';
       //inject: [ConfigService],
       //useFactory: (cfg: ConfigService): RedisModuleOptions => ({
       type: 'single',
-      url: `redis://${ENV_VAR.REDIS_HOST}:${ENV_VAR.REDIS_PORT}/${ENV_VAR.REDIS_DB}`,
+      url: REDIS_CONNECTION_URL,
     }),
     //}),
 
