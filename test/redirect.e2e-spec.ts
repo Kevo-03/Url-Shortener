67bf96b67532f52b58784ee95a318f2dec4f3660
Kıvanç Onat Türker <kivo0326@gmail.com>
2025-07-16 14:07:45 +0300

    separated tests for redirect and shorten

---
+++ test/redirect.e2e-spec.ts
@@ -0,0 +1,47 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication, VersioningType } from '@nestjs/common';
+import * as request from 'supertest';
+import { App } from 'supertest/types';
+import { AppModule } from './../src/app.module';
+import { ENV_VAR } from '../src/config/app.config';
+
+describe('Redirection System', () => {
+    let app: INestApplication<App>;
+    let shortCode: string;
+
+    const USER_NAME = ENV_VAR.BASIC_AUTH_USER;
+    const PASSWORD = ENV_VAR.BASIC_AUTH_PASS;
+
+    beforeEach(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
+
+        app = moduleFixture.createNestApplication();
+        app.enableVersioning({ type: VersioningType.URI });
+        await app.init();
+        const res = await request(app.getHttpServer())
+            .post('/v1/shorten')
+            .auth(USER_NAME!, PASSWORD!)
+            .send({ url: 'https://example.com/very/long/path' })
+            .expect(201)
+        shortCode = res.body.shortCode;
+    });
+
+    it('should redirecs to the original Url mapped to the short Url', async () => {
+        const code = shortCode;
+
+        const res = await request(app.getHttpServer())
+            .get(`/${code}`)
+            .redirects(0)
+            .expect(301)
+
+        expect(res.headers.location).toBe('https://example.com/very/long/path',);
+    });
+
+    it('should return 404 for non-existing short Url', async () => {
+        return request(app.getHttpServer())
+            .get('/abcdser')
+            .expect(404)
+    });
+});
