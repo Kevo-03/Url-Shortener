53a8b1959936063579f8f61d7087974706ac9310
Kıvanç Onat Türker <kivo0326@gmail.com>
2025-07-18 17:24:33 +0300

    checking whole header at once in guard

--- src/url-shortener/auth/auth.guard.ts
+++ src/url-shortener/auth/auth.guard.ts
@@ -5,8 +5,11 @@ import { Request } from 'express';
 import { ENV_VAR } from '../../config/app.config';
 import { Buffer } from 'buffer';
 
+const expected = (ENV_VAR.BASIC_AUTH ?? '').trim();
+
 @Injectable()
 export class BasicAuthGuard implements CanActivate {
+
     canActivate(ctx: ExecutionContext) {
         const req = ctx.switchToHttp().getRequest<Request>();
         const header = req.headers.authorization;
@@ -15,14 +18,17 @@ export class BasicAuthGuard implements CanActivate {
             throw new UnauthorizedException('Missing Authorization header');
         }
 
-        const [user, pass] = Buffer
-            .from(header.slice(6), 'base64')
-            .toString('utf8')
-            .split(':', 2);
+        /*  const [user, pass] = Buffer
+             .from(header.slice(6), 'base64')
+             .toString('utf8')
+             .split(':', 2);
+ 
+         if (user === ENV_VAR.BASIC_AUTH_USER && pass === ENV_VAR.BASIC_AUTH_PASS) {
+             return true;
+         } */
+        if (header?.trim() === expected) return true;
 
-        if (user === ENV_VAR.BASIC_AUTH_USER && pass === ENV_VAR.BASIC_AUTH_PASS) {
-            return true;
-        }
+
         throw new UnauthorizedException({
             statusCode: 401,
             message: 'Invalid basic credentials',
